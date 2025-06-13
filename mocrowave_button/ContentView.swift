11d24bd2e694bf48a4edad8b42e4d6a0c20638a2
jason <pochli6262@gmail.com>
2025-06-13 12:00:53 +0800

    Initial Commit

---
+++ mocrowave_button/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mocrowave_button
+//
+//  Created by 廖柏鈞 on 2025/6/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mocrowave_button/mocrowave_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  mocrowave_buttonApp.swift
+//  mocrowave_button
+//
+//  Created by 廖柏鈞 on 2025/6/13.
+//
+
+import SwiftUI
+
+@main
+struct mocrowave_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
